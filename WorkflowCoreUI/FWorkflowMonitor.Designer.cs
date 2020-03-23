4fe141c0e39458720ae33e2d8e264393e6ef646e
Anton Geo <antongeorgescu@gmail.com>
2020-03-23 18:02:00 -0400

    Added some code to Workflows Monitoring form I(script lines to be run in the database)

---
+++ WorkflowCoreUI/FWorkflowMonitor.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WorkflowCoreUI
+{
+    partial class FWorkflowMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvwWorkflows;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(546, 319);
+            this.textBox1.TabIndex = 0;
+            // 
+            // FWorkflowMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 388);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "FWorkflowMonitor";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WorkflowCoreUI/FWorkflowMonitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkflowCoreUI
+{
+    public partial class FWorkflowMonitor : Form
+    {
+        public FWorkflowMonitor()
+        {
+            InitializeComponent();
+
+            //dgvwWorkflows.DataSource = new List<object>
+            //{
+            //    new { Id = 1, Name = "The Very Big Corporation of America" },
+            //    new { Id = 2, Name = "Old Accountants Ltd" }
+            //};
+
+            textBox1.AppendText("Copy the following 2 lines of Sql script and run them where WorkflowCore persistance database is deployed.");
+            textBox1.AppendText($"{Environment.NewLine}If you leave @WorkflowId = -1, you will see the latest workflow status.");
+            textBox1.AppendText($"{Environment.NewLine}Otherwise you can change the parameter to an actual WorkflowId that ran before...");
+            textBox1.AppendText($"{Environment.NewLine}");
+            textBox1.AppendText($"{Environment.NewLine}EXEC [WorkflowCore].[dbo].[GetWorkflowStatus] @WorkflowId = -1");
+            textBox1.AppendText($"{Environment.NewLine}GO");
+        }
+    }
+}
